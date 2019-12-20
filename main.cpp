8b05929434cbc582c1da512da2f19eb707f6db61
Konstantin Kulikov <rasmadeus@gmail.com>
2019-12-20 21:25:53 +0100

    Add any order

--- main.cpp
+++ main.cpp
@@ -10,6 +10,20 @@ using LastName = NamedType<std::string, struct LastNameTag>;
 static const FirstName::argument firstName;
 static const LastName::argument lastName;
 
+template<typename TypeToPick, typename... Types>
+TypeToPick pick(Types&&... args)
+{
+    return std::get<TypeToPick>(std::make_tuple(std::forward<Types>(args)...));
+}
+
+template<typename Arg0, typename Arg1>
+void printName(Arg0&& arg0, Arg1&& arg1)
+{
+    auto const firstName = pick<FirstName>(arg0, arg1);
+    auto const lastName = pick<LastName>(arg0, arg1);
+    std::cout << firstName.get() << "\t" << lastName.get() << "\n";
+}
+
 void print(FirstName const& firstName, LastName const& lastName)
 {
     std::cout << firstName.get() << "\t" << lastName.get() << "\n";
@@ -18,5 +32,7 @@ void print(FirstName const& firstName, LastName const& lastName)
 int main(int argc, char** argv)
 {
     print(firstName = "James", lastName = "Bond");
+    printName(firstName = "James", lastName = "Bond");
+    printName(lastName = "Bond", firstName = "James");
     return 0;
 }
